da5e450699eb5f670869e144518a8745d8ab8ac3
Morooooo <102239793+Morooooo@users.noreply.github.com>
2023-07-29 12:32:39 +0300

    Update server.js
    Passed purchased items info to the checkout page

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+
 require ('dotenv').config()
 const session=require('express-session')
 const express=require('express')
@@ -7,50 +8,40 @@ const paypal=require('@paypal/checkout-server-sdk')
 const Environment=process.env.NODE_ENV=== 'production'?
 paypal.core.LiveEnvironment:paypal.core.SandboxEnvironment;
 var data=[]
+var total;
 const paypalClient=new paypal.core.PayPalHttpClient(
     new Environment(
         process.env.PAYPAL_CLIENT_ID,
         process.env.PAYPAL_CLIENT_SECRET))
-const storeItems=new Map([
-  [1,{name:'CSKA_FC-cap',price:22}],
-  [2,{name:'Slavia_FC-cap',price:13}],
-  [3,{name:'Beroe_FC-cap',price:12}]  
-])
+const storeItems=new Map([ [1,{name:'CSKA_FC-cap',price:22}], [2,{name:'Slavia_FC-cap',price:13}],  [3,{name:'Beroe_FC-cap',price:12}]  ])
 var selectedItems=[]
 app.set('view engine','ejs');
 app.use(express.static('public'))
 app.use(express.json())
-app.use(session({
-    secret:process.env.PAYPAL_CLIENT_SECRET,
-    resave:false,
-    saveUninitialized:true
-}))
+app.use(session({  secret:process.env.PAYPAL_CLIENT_SECRET, resave:false,saveUninitialized:true}))
 
 app.get('/',(req,res)=>{
-    res.render('index',{paypalClientId:process.env.PAYPAL_CLIENT_ID
-       })
+    res.render('index',{paypalClientId:process.env.PAYPAL_CLIENT_ID,cartData:data })
 })
+
 app.get('/:params',(req,res)=>{
-    res.render(req.url.split('/')[1],{paypalClientId:process.env.PAYPAL_CLIENT_ID
-    })
-})
+    res.render(req.url.split('/')[1],{paypalClientId:process.env.PAYPAL_CLIENT_ID,cartData:data,storeItems:storeItems})
+}) 
 app.get('/images/:params',(req,res)=>{
     const image=req.url.split('/images/')[1]
     res.sendFile(`${__dirname}/public/images/${image}`)
-    
 })
 app.post('/addToCart',(req,res)=>{ 
 selectedItems.push(req.body.items[0])
 req.session.shoppingData=selectedItems
-console.log(req.session.shoppingData)
 data=req.session.shoppingData
-
-})
+ /**/ console.log(data)
+res.redirect('/') })
 app.post('/create-order',async (req,res)=>{
 const request=new paypal.orders.OrdersCreateRequest()
 const selectedItems=req.session.shoppingData||[];
-//console.log(session.shoppingData)
-const total=data.reduce((sum,item)=>{   
+
+ total=data.reduce((sum,item)=>{   
 return sum +  storeItems.get(Number(item.id)).price * item.quantity},0)
 request.prefer("return=representation");
 request.requestBody({
@@ -64,23 +55,15 @@ request.requestBody({
                 item_total:{
                     currency_code:'USD',
                     value:total
-                }
-
-            }
-          } ,
+                }  }  } ,
            items:selectedItems.map(item=>{
             const storeItem=storeItems.get(item.id)
             return {
                 name:storeItem.name,
                 unit_amount:{
                     currency_code:'USD',
-                    value:storeItem.price
-                } ,
-            quantity:item.quantity
-
-            }
-            
-           })
+                    value:storeItem.price  } ,
+            quantity:item.quantity }  })
         }
     ]
 
@@ -88,6 +71,9 @@ request.requestBody({
 try {
     const order=await paypalClient.execute(request)
     res.json({id:order.result.id})
+    data.length=0;
+    console.log(data)
+
     
 } catch (e) {
     res.status(500).json({error:e.message})
